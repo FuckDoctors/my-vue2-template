8c7cb5062d400ca95ac31e7844d34fa95f665415
ZhaoBin <zhaobin@ZhaoBindeiMac.local>
2017-09-05 20:07:17 +0800

    添加dotfiles

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    // "autoprefixer": {}
+    'postcss-import': {},
+    'postcss-mixins': {}, // Note, that you must set this plugin before postcss-simple-vars and postcss-nested.
+    'postcss-simple-vars': {}, // 开启postcss-simple-vars，不然css中的单独的$变量无法解析
+    'postcss-nested': {},
+    'postcss-cssnext': {
+      browsers: ['last 2 versions']
+    }
+  }
+}
